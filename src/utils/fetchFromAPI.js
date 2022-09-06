b03c2b68e7c60420a6d0c0bb088305f86578effb
ayush-dhanker <ayush.dhanker@gmail.com>
2022-09-06 23:10:40 +0530

    Initial Commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "49129b7a0emsh0a98431a8646483p122a1cjsnf7906ceb8e5c",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
